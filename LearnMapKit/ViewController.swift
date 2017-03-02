70509bb4d2836c9d32b2ba647e63807bc36f3677
Nguyen Van Thieu B <nguyen.van.thieub@tms-Mac-mini.local>
2017-03-02 15:30:08 +0700

    Pin View

--- LearnMapKit/ViewController.swift
+++ LearnMapKit/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
     var locationManager = CLLocationManager()
@@ -20,23 +20,52 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
+        mapView.delegate = self
         mapView.showsUserLocation = true
         
         if let currentLocation = locationManager.location?.coordinate {
-            let annotation = CustomAnnotation(title: "KeangNam", subtitle: "Pham Hung", coordinate: CLLocationCoordinate2D(latitude: currentLocation.latitude, longitude: currentLocation.longitude))
+            let image = UIImage(named: "framgia")!
+            let newImage = imageWithImage(image: image, scaledToSize: CGSize(width: 20, height: 20))
+            let annotation = CustomAnnotation(title: "Framiga", subtitle: "13F Keang Nam", coordinate: CLLocationCoordinate2D(latitude: currentLocation.latitude, longitude: currentLocation.longitude), image: newImage)
             mapView.addAnnotation(annotation)
         }
-        
+    }
+    
+    func imageWithImage(image:UIImage, scaledToSize newSize:CGSize) -> UIImage{
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0);
+        image.draw(in: CGRect(origin: CGPoint.zero, size: CGSize(width: newSize.width, height: newSize.height)))
+        let newImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return newImage
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let currentLocation = locations.last
         let region = MKCoordinateRegion(center: (currentLocation?.coordinate)!, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
         mapView.setRegion(region, animated: true)
+        locationManager.stopUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if let myAnnotation = annotation as? CustomAnnotation {
+            var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: "CustomPinannotationView")
+            if pinView == nil {
+                pinView = MKAnnotationView(annotation: myAnnotation, reuseIdentifier: "CustomPinannotationView")
+                pinView?.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+                pinView?.canShowCallout = true
+                pinView?.calloutOffset = CGPoint(x: 0, y: 4)
+                pinView?.contentMode = .center
+            } else {
+                pinView?.annotation = annotation
+            }
+            pinView?.image = myAnnotation.image
+            return pinView
+        }
+        return nil
     }
 
 }
